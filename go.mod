1b236315036803ed8b5d2352a5cd0550a9aae77e
mrmtsu <81202muramatsu@gmail.com>
2020-12-30 18:41:55 +0900

    Top page creation

---
+++ go.mod
@@ -0,0 +1,13 @@
+module go-blog
+
+go 1.15
+
+require (
+	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/flosch/pongo2 v0.0.0-20200913210552-0d938eb266f3
+	github.com/labstack/echo v3.3.10+incompatible
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/stretchr/testify v1.6.1 // indirect
+	github.com/valyala/fasttemplate v1.2.1 // indirect
+	golang.org/x/crypto v0.0.0-20201221181555-eec23a3978ad // indirect
+)
---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/flosch/pongo2"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
+)
+
+const tmplPath = "src/template/"
+
+var e = createMax()
+
+func main() {
+	e.GET("/", articleIndex)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func createMax() *echo.Echo {
+	e := echo.New()
+
+	e.Use(middleware.Recover())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Gzip())
+
+	return e
+}
+
+func articleIndex(c echo.Context) error {
+	data := map[string]interface{}{
+		"Message": "Hello, World!",
+		"Now":     time.Now(),
+	}
+	return render(c, "article/index.html", data)
+}
+
+func htmlBlob(file string, data map[string]interface{}) ([]byte, error) {
+	return pongo2.Must(pongo2.FromCache(tmplPath + file)).ExecuteBytes(data)
+}
+
+func render(c echo.Context, file string, data map[string]interface{}) error {
+	b, err := htmlBlob(file, data)
+	if err != nil {
+		return c.NoContent(http.StatusInternalServerError)
+	}
+	return c.HTMLBlob(http.StatusOK, b)
+}

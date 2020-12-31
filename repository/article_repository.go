5e06365cc2bba3a00fac181c68fee9d220d7023c
mrmtsu <81202muramatsu@gmail.com>
2020-12-31 10:04:18 +0900

    db

---
+++ repository/article_repository.go
@@ -0,0 +1,14 @@
+package repository
+
+import "go-blog/model"
+
+func ArticleList() ([]*model.Article, error) {
+	query := `SELECT * FROM articles;`
+
+	var articles []*model.Article
+	if err := db.Select(&articles, query); err != nil {
+		return nil, err
+	}
+
+	return articles, nil
+}
